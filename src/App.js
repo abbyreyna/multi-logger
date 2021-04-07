6aaf233aad658e14206417037e6a87160dd400ff
abbyreyna <reyna.abby08@gmail.com>
2021-04-06 21:03:54 -0700

    Edit Function & User Modal components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddUserModal from './components/users/AddUserModal';
+import UserListModal from './components/users/UserListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -19,6 +22,9 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddUserModal />
+        <UserListModal />
         <Logs />
       </div>
     </Fragment>
