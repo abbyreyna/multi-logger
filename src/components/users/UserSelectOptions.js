a42cc71778951b616b15b9d9272eb87e2c35e334
abbyreyna <reyna.abby08@gmail.com>
2021-04-22 20:18:41 -0700

    User select options component

---
+++ src/components/users/UserSelectOptions.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getUsers } from '../../actions/userActions';
+
+const UserSelectOptions = ({ getUsers, user: { users, loading } }) => {
+  useEffect(() => {
+    getUsers();
+    // eslint-disable-next-line
+  }, []);
+  return (
+    !loading &&
+    users !== null &&
+    users.map((u) => (
+      <option key={u.id} value={`${u.firstName} ${u.lastName}`}>
+        {u.firstName} {u.lastName}
+      </option>
+    ))
+  );
+};
+
+UserSelectOptions.propTypes = {
+  user: PropTypes.object.isRequired,
+  getUsers: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps, { getUsers })(UserSelectOptions);
